ad92d4dd371de1a48af4e7faa146b0c6f2179837
Irfan Khan <irfankhan501@outlook.com>
2020-12-04 13:15:08 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,88 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+let studentArr = require("./InitialData");
+const port = 8080;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // your code goes here
 
+app.get("/api/student", (req, res) => {
+  res.send(studentArr);
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get("/api/student/:id", (req, res) => {
+  const student = studentArr.filter((s) => s.id == req.params.id);
+  if (student.length === 1) {
+    res.send(student);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-module.exports = app;   
+app.post("/api/student", (req, res) => {
+  const { name, currentClass, division } = req.body;
+  console.log(req.body);
+  if (req.body != {}) {
+    if (name !== "" && currentClass !== "" && division !== "") {
+      const id = studentArr.length + 1;
+      const student = {
+        id,
+        name,
+        currentClass,
+        division,
+      };
+      studentArr.push(student);
+      res.send({ id });
+    }
+    res.sendStatus(400);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.put("/api/student/:id", (req, res) => {
+  const student = studentArr.filter((s) => s.id == req.params.id);
+//   console.log(req.body);
+  if (req.body !== {} && student.length == 1) {
+    const { name, currentClass, division } = req.body;
+
+    if (name != undefined && name != "") {
+      student.name = name;
+    }
+    if (currentClass != undefined && currentClass != "") {
+      student.currentClass = currentClass;
+    }
+    if (division != undefined && division != "") {
+      student.division = division;
+    }
+    let newStudentArr = studentArr.filter((s) => s.id != req.params.id);
+    newStudentArr.push(student);
+    studentArr = [...newStudentArr];
+    res.send({ name: student.name });
+  } else {
+    console.log("in");
+    //   res.status(400);
+    res.sendStatus(400);
+  }
+});
+
+app.delete("/api/student/:id", (req, res) => {
+  const student = studentArr.filter((s) => s.id == req.params.id);
+  console.log(student);
+  if (student.length === 1) {
+    const newStudentArr = studentArr.filter((s) => s.id != req.params.id);
+    studentArr = newStudentArr;
+    res.send("ok");
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+module.exports = app;
